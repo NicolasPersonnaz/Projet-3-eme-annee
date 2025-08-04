e41a517299023a6a01ef362b500c51d621e247fe
Nicolas Personnaz <117669784+NicolasPersonnaz@users.noreply.github.com>
2025-08-04 12:04:58 +0200

    Update panel.ps1

--- panel.ps1
+++ panel.ps1
@@ -1,3 +1,6 @@
+# Script pour faire en sorte que l'écran tactile fonctionne bien en se connecectant à distance sur une tablette tactile windows 11
+
+
 # Chemin de la clé de registre pour TabletTip
 $keyPath = "HKCU:\Software\Microsoft\TabletTip\1.7"
 # Liste des valeurs à définir dans cette clé
@@ -30,4 +33,5 @@ foreach ($value in $values) {
     Write-Output "La valeur $value a été définie sur 1 dans $keyPath"
 }
  
+
 Write-Output "Configuration des valeurs de registre terminée."
